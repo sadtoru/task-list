de3f6b738075e16d527bbb31c05c6c4acb4c885c
DaianaChile <daiarocio@outlook.com>
2022-10-08 00:28:05 -0300

    sexto commit

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TASKS } from 'src/app/mock-tasks';
+import { Task } from 'src/app/task';
 
 @Component({
   selector: 'app-tasks',
@@ -6,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
+  
+  tasks: Task[] = TASKS;
 
   constructor() { }
 
