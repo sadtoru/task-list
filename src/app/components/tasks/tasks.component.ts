0ef7785e75c29ffe0844f02d33092f19598bbf76
DaianaChile <daiarocio@outlook.com>
2022-10-08 21:31:52 -0300

    13

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -40,9 +40,9 @@ export class TasksComponent implements OnInit {
  }
 
  addTask(task: Task){
-  this.taskService.addTask(task).subscribe((task) => {
-    this.tasks.push.(task)
-  });
+  this.taskService.addTask(task).subscribe((task) =>{
+    this.tasks.push(task);
+  }) 
  }
 
 }
--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -34,4 +34,8 @@ private apiUrl = 'http://localhost:5001/tasks'
     return this.http.put<Task>(url, task, httpOptions);
 
   }
+
+  addTask(task: Task) : Observable<Task>{
+    return this.http.post<Task>(this.apiUrl, task, httpOptions);
+  }
 }
